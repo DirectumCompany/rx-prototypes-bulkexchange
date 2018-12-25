b5159502752357f6a62cdfb4ea1af7a11b8db7e5
Замеров Александр (Zamerov_AY) <Zamerov_AY@directum.ru>
2018-12-25 14:11:13 +0400

    #76778 Добавил поддержку DCX и парсинг номера заказа из XML. Добавил пример документа. [Rx]

--- src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/Sungero.Exchange/ModuleServerFunctions.cs
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/Sungero.Exchange/ModuleServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -8,6 +8,27 @@ namespace Sungero.BulkExchangeSolution.Module.Exchange.Server
 {
   partial class ModuleFunctions
   {
-
+    protected override Sungero.Docflow.IOfficialDocument GetOrCreateNewExchangeDocument(Sungero.ExchangeCore.IBoxBase box, object clientUntyped, object documentUntyped, Sungero.Parties.ICounterparty sender, string serviceCounterpartyId, DateTime messageDate, bool isIncoming)
+    {
+      var document = base.GetOrCreateNewExchangeDocument(box, clientUntyped, documentUntyped, sender, serviceCounterpartyId, messageDate, isIncoming);
+      if (FinancialArchive.UniversalTransferDocuments.Is(document) || FinancialArchive.Waybills.Is(document))
+      {
+        var serviceDocument = documentUntyped as NpoComputer.DCX.Common.IDocument;
+        var xdoc = System.Xml.Linq.XDocument.Load(new System.IO.MemoryStream(serviceDocument.Content));
+        RemoveNamespaces(xdoc);
+        var additionalProperties = xdoc.Descendants("ТекстИнф");
+        if (additionalProperties.Any())
+        {
+          var purchaseOrderElement = additionalProperties.FirstOrDefault(i => (string)i.Attribute("Идентиф") == "номер_заказа");
+          if (purchaseOrderElement != null)
+          {
+            var exchangeDocumentInfo = ExchangeDocumentInfos.As(Sungero.Exchange.PublicFunctions.ExchangeDocumentInfo.GetExDocumentInfoByExternalId(box, serviceDocument.ServiceEntityId));
+            exchangeDocumentInfo.PurchaseOrder = purchaseOrderElement.Attribute("Значен").Value;
+            exchangeDocumentInfo.Save();
+          }
+        }
+      }
+      return document;
+    }
   }
 }
