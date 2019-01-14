00f9275245c6f8ad6c5d7aa73a8eb444d72af5ad
Соломенникова Надежда (Solomennikova_NV) <Solomennikova_NV@directum.ru>
2019-01-14 13:00:13 +0400

    Немного логгирования и проверка сертификата #76783

--- src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.ClientBase/ModuleClientFunctions.cs
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.ClientBase/ModuleClientFunctions.cs
@@ -10,21 +10,30 @@ namespace Sungero.BulkExchangeSolution.Client
   {
     public virtual void SignCheckedDocuments()
     {
-      var checkedSets = Sungero.BulkExchangeSolution.Module.Exchange.Functions.Module.Remote.GetCheckedSets();
+      var checkedSets = Module.Exchange.Functions.Module.Remote.GetCheckedSets().Where(c => BusinessUnitBoxes.As(c.RootBox).SignDocumentCertificate != null &&
+                                                                                                                    Equals(BusinessUnitBoxes.As(c.RootBox).SignDocumentCertificate.Owner, Users.Current));
       var messageIds = checkedSets.Select(x => x.ServiceMessageId).Distinct().ToList();
       foreach (var messageId in messageIds)
       {
         var infos = checkedSets.Where(x => x.ServiceMessageId == messageId);
-        Logger.DebugFormat("Sign document set with document ids {0}", string.Join(", ", infos.Select(i => i.Document.Id)));
+        Logger.DebugFormat("Start sign document set with document ids {0}.", string.Join(", ", infos.Select(i => i.Document.Id)));
         foreach (var info in infos)
         {
           if (info.NeedSign == true)
           {
             var certificate = BusinessUnitBoxes.As(info.RootBox).SignDocumentCertificate;
-            if (Docflow.PublicFunctions.OfficialDocument.ApproveWithAddenda(info.Document, null, certificate, "Автоподпись", null, false, null))
+            try
             {
-              info.SignStatus = Sungero.BulkExchangeSolution.ExchangeDocumentInfo.SignStatus.Signed;
-              info.Save();
+              if (Docflow.PublicFunctions.OfficialDocument.ApproveWithAddenda(info.Document, null, certificate, "Автоподпись", null, false, null))
+              {
+                Logger.DebugFormat("Sign document set with document ids {0} successfully.", string.Join(", ", info.Document.Id));
+                info.SignStatus = Sungero.BulkExchangeSolution.ExchangeDocumentInfo.SignStatus.Signed;
+                info.Save();
+              }
+            }
+            catch (Exception ex)
+            {
+              Logger.Error(Sungero.BulkExchangeSolution.Resources.CannotSignDocument, ex);
             }
           }
         }
