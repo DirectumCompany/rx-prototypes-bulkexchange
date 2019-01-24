cd0d81a2a101fb454772e99ad239bd7c47018d2a
Замеров Александр (Zamerov_AY) <Zamerov_AY@directum.ru>
2019-01-24 18:18:36 +0400

    #77685 #77969 Определяем ответственного за договор и ругаемся, если его нет. [Rx]

--- src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/BusinessUnitBox/BusinessUnitBoxServerFunctions.cs
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/BusinessUnitBox/BusinessUnitBoxServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,38 @@ namespace Sungero.BulkExchangeSolution.Server
 {
   partial class BusinessUnitBoxFunctions
   {
-
+    public override Sungero.Company.IEmployee GetExchangeDocumentResponsible(Sungero.Parties.ICounterparty counterparty, List<Sungero.Exchange.IExchangeDocumentInfo> infos)
+    {
+      return GetResponsibleForAutoRouting(_obj, counterparty, infos);
+    }
+    
+    public static Sungero.Company.IEmployee GetResponsibleForAutoRouting(ExchangeCore.IBoxBase box, Sungero.Parties.ICounterparty counterparty, List<Sungero.Exchange.IExchangeDocumentInfo> infos)
+    {
+      if (infos != null)
+      {
+        var documentSets = Functions.ExchangeDocumentInfo.GetDocumentSets(infos.OfType<IExchangeDocumentInfo>().ToList());
+        foreach (var documentSet in documentSets.Where(s => s.IsFullSet))
+        {
+          if (documentSet.Type == Constants.Exchange.ExchangeDocumentInfo.DocumentSetType.ContractStatement)
+          {
+            // Раз комплект корректный - значит номер один и на него существует один договор.
+            var contractNumber = documentSet.ExchangeDocumentInfos
+              .Select(i => i.ContractNumber)
+              .FirstOrDefault();
+            var contract = Sungero.Contracts.ContractBases
+              .GetAll(c => Equals(counterparty, c.Counterparty) && c.RegistrationNumber == contractNumber)
+              .FirstOrDefault();
+            if (contract != null && contract.ResponsibleEmployee != null)
+              return contract.ResponsibleEmployee;
+          }
+        }
+      }
+      
+      var company = Parties.CompanyBases.As(counterparty);
+      if (company != null && company.Responsible != null)
+        return company.Responsible;
+      else
+        throw AppliedCodeException.Create(string.Format("Не указан ответственный за контрагента {0}", counterparty.Name));
+    }
   }
 }
--- src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/DepartmentBox/DepartmentBoxServerFunctions.cs
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/DepartmentBox/DepartmentBoxServerFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,9 @@ namespace Sungero.BulkExchangeSolution.Server
 {
   partial class DepartmentBoxFunctions
   {
-
+    public override Sungero.Company.IEmployee GetExchangeDocumentResponsible(Sungero.Parties.ICounterparty counterparty, List<Sungero.Exchange.IExchangeDocumentInfo> infos)
+    {
+      return Functions.BusinessUnitBox.GetResponsibleForAutoRouting(_obj, counterparty, infos);
+    }
   }
 }
