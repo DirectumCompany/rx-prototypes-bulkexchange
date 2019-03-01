23f70bccd6850115e1c8fac56f20ba3940a247e9
Соломенникова Надежда (Solomennikova_NV) <Solomennikova_NV@directum.ru>
2019-03-01 15:17:43 +0400

    Реестр поступивших документов #78761

--- src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/Sungero.FinancialArchiveUI/ModuleHandlers.cs
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/Sungero.FinancialArchiveUI/ModuleHandlers.cs
@@ -6,6 +6,84 @@ using Sungero.CoreEntities;
 
 namespace Sungero.BulkExchangeSolution.Module.FinancialArchiveUI.Server
 {
+  partial class IncomingDocumentsFromServiceFolderHandlers
+  {
+
+    public virtual IQueryable<Sungero.Docflow.IOfficialDocument> IncomingDocumentsFromServiceDataQuery(IQueryable<Sungero.Docflow.IOfficialDocument> query)
+    {
+      var infos = Sungero.Exchange.ExchangeDocumentInfos.GetAll(i => i.MessageType == Sungero.Exchange.ExchangeDocumentInfo.MessageType.Incoming);
+      
+      if (_filter == null)
+        return query.Where(d => infos.Select(i => i.Document).Contains(d));
+      
+      #region Фильтры
+      
+      // Фильтр "Наша организация".
+      if (_filter.BusinessUnit != null)
+        query = query.Where(d => Equals(d.BusinessUnit, _filter.BusinessUnit));
+      
+      // Фильтр "Подразделение".
+      if (_filter.Department != null)
+        query = query.Where(d => Equals(d.Department, _filter.Department));
+      
+      // Фильтр "Ответсвенный"
+      if (_filter.Responsible != null)
+        query = query.Where(d => Docflow.AccountingDocumentBases.Is(d) && Equals(Docflow.AccountingDocumentBases.As(d).ResponsibleEmployee, _filter.Responsible) ||
+                            Docflow.ContractualDocumentBases.Is(d) && Equals(Sungero.Contracts.ContractualDocuments.As(d).ResponsibleEmployee, _filter.Responsible));
+      
+      // Фильтр "Контрагент".
+      if (_filter.Counterparty != null)
+        infos = infos.Where(i => Equals(i.Counterparty, _filter.Counterparty));
+      
+      #region Фильтрация по дате договора
+
+      DateTime? beginDate = null;
+      DateTime? endDate = null;
+      var currentDate = Calendar.UserToday;
+      
+      if (_filter.Today)
+      {
+        beginDate = currentDate;
+        endDate = currentDate;
+      }
+
+      if (_filter.ThreeDays)
+      {
+        beginDate = currentDate.AddDays(-1);
+        endDate = currentDate.AddDays(1);
+      }
+      if (_filter.Week)
+      {
+        beginDate = currentDate.BeginningOfWeek();
+        endDate = currentDate.EndOfWeek();
+      }
+      if (_filter.ManualPeriod)
+      {
+        if (_filter.DateRangeFrom.HasValue)
+          beginDate = _filter.DateRangeFrom.Value;
+        if (_filter.DateRangeTo.HasValue)
+          endDate = _filter.DateRangeTo.Value;
+      }
+
+      if (beginDate != null)
+        query = query.Where(d => d.Created >= beginDate.Value.BeginningOfDay());
+      
+      if (endDate != null)
+        query = query.Where(d => d.Created <= endDate.Value.EndOfDay());
+            
+      #endregion
+      
+      // Фильтр "Тип документа"
+      if (_filter.Accounting || _filter.Contractual || _filter.Other)
+        query = query.Where(d => (_filter.Accounting && Docflow.AccountingDocumentBases.Is(d)) ||
+                            (_filter.Contractual && Docflow.ContractualDocumentBases.Is(d)) ||
+                            (_filter.Other && !Docflow.AccountingDocumentBases.Is(d) && !Docflow.ContractualDocumentBases.Is(d)));
+      #endregion
+      
+      return query.Where(d => infos.Select(i => i.Document).Contains(d));
+    }
+  }
+
   partial class OutgoingDocumentsForSignatureFolderHandlers
   {
 
@@ -18,7 +96,7 @@ namespace Sungero.BulkExchangeSolution.Module.FinancialArchiveUI.Server
         .Distinct()
         .ToList();
       var exchangeDocuments = Sungero.Exchange.ExchangeDocumentInfos.GetAll(i => assignmentsDocuments.Contains(i.Document)).Select(d => d.Document).ToList();
- 
+      
       return query = query.Where(x => assignmentsDocuments.Contains(x) && !exchangeDocuments.Contains(x));
     }
   }
