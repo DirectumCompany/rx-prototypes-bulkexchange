b3e9f1b33f904a3db6a063603181c571780f8c25
Назипова Наталья (Nazipova_NA) <Nazipova_NA@directum.ru>
2020-02-10 12:21:26 +0400

    #96842 Актуалихация на 3.3

---
+++ src/Packages/Sungero.BulkExchangeSolution/Sungero.BulkExchangeSolution.Server/Sungero.FinancialArchiveUI/ModuleAsyncHandlers.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace Sungero.BulkExchangeSolution.Module.FinancialArchiveUI.Server
+{
+  partial class ModuleAsyncHandlers 
+  {
+
+  }
+}
